91b2362de7951231f96193a20c3122ce3cc3999e
MSI\Leo Montegrande <lmontegrande91@gmail.com>
2017-12-03 20:50:39 -0800

    init

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+	
+	// Update is called once per frame
+	void Update () {
+        if (target != null)
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour {
+	
+    private PhotonView _photonView;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _photonView = GetComponent<PhotonView>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (_photonView.isMine) {
+            Camera.main.GetComponent<CameraFollow>().target = gameObject;
+        } else {
+            _rigidbody2D.isKinematic = true;
+        }
+    }
+
+    void Update () {
+		if (_photonView.isMine)
+        {
+            _rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+	}
+}
